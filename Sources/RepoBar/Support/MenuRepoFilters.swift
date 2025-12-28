064a04fc5f542bd478de1f09cc72ab3eb37a78b6
Peter Steinberger <steipete@gmail.com>
2025-12-28 19:12:59 +0100

    refactor: simplify menu filter options

--- Sources/RepoBar/Support/MenuRepoFilters.swift
+++ Sources/RepoBar/Support/MenuRepoFilters.swift
@@ -15,15 +15,11 @@ import RepoBarCore
 enum MenuRepoFilter: String, CaseIterable, Hashable {
     case all
     case work
-    case issues
-    case prs
 
     var label: String {
         switch self {
         case .all: "All"
         case .work: "Work"
-        case .issues: "Issues"
-        case .prs: "PRs"
         }
     }
 
@@ -33,10 +29,6 @@ enum MenuRepoFilter: String, CaseIterable, Hashable {
             return .none
         case .work:
             return RepositoryOnlyWith(requireIssues: true, requirePRs: true)
-        case .issues:
-            return RepositoryOnlyWith(requireIssues: true)
-        case .prs:
-            return RepositoryOnlyWith(requirePRs: true)
         }
     }
 }
